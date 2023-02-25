bdb0e0a46e67023a5c74c2f5a65705d5ff9aca33
CarmineNapolitano <carmine-2000@outlook.it>
2023-02-25 15:52:27 +0100

    Comments added and image fixed

--- contracts/HealthCoin.sol
+++ contracts/HealthCoin.sol
@@ -9,6 +9,7 @@ contract HealthCoin is ERC20, AccessControl {
    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
    address destinatario;
 
+   //mapping che associa il prezzo ad un item
    mapping(uint => uint) private pricesMapping;
 
    constructor(
@@ -34,47 +35,72 @@ contract HealthCoin is ERC20, AccessControl {
    event ChecksoloAdmin(address user);
    event ChecksoloMinters(address user);
 
-   modifier soloAdmin() {
+   // Modificatore soloAdmin
+modifier soloAdmin() {
+      // Emette un evento ChecksoloAdmin che registra il mittente
       emit ChecksoloAdmin(msg.sender);
+      // Controlla se il mittente è un amministratore
       require(isAdmin(msg.sender), "Restricted to admins");
+      // Procede con l'operazione 
       _;
    }
 
-   modifier soloMinters() {
+// Modificatore soloMinters
+modifier soloMinters() {
+      // Emette un evento ChecksoloMinters che registra il mittente
       emit ChecksoloMinters(msg.sender);
+      // Controlla se il mittente è un minter
       require(isMinter(msg.sender), "Caller is not a minter");
+      // Procede con l'operazione 
       _;
    }
 
-   function isAdmin(address account) public virtual view returns (bool) {
+// Funzione per verificare se un account è un amministratore
+function isAdmin(address account) public virtual view returns (bool) {
+      // Verifica se l'account ha il ruolo di amministratore predefinito
       return hasRole(DEFAULT_ADMIN_ROLE, account);
    }
 
-   function isMinter(address account) public virtual view returns (bool) {
+// Funzione per verificare se un account è un minter
+function isMinter(address account) public virtual view returns (bool) {
+      // Verifica se l'account ha il ruolo di minter
       return hasRole(MINTER_ROLE, account);
    }
 
-   function mint(address to, uint256 amount) public soloMinters {
+// Funzione di coniazione
+function mint(address to, uint256 amount) public soloMinters {
+      // Esegue la coniazione
       _mint(to, amount);
     }
 
-    function addMinterRole(address to) public soloAdmin {
+// Funzione per aggiungere un nuovo minter
+function addMinterRole(address to) public soloAdmin {
+      // Emette un evento di debug per registrare l'operazione
       emit Debug(to, msg.sender, MINTER_ROLE, getRoleAdmin(MINTER_ROLE), DEFAULT_ADMIN_ROLE);
+      // Assegna il ruolo di minter all'account
       grantRole(MINTER_ROLE, to);
     }
 
-    function removeMinterRole (address to) public {
+// Funzione per rimuovere il ruolo di minter da un account
+function removeMinterRole (address to) public {
+      // Rimuove il ruolo di minter dall'account
       renounceRole(MINTER_ROLE, to);
     }
 
-    function setDestinatario (address to) public soloAdmin{
+// Funzione per impostare un destinatario
+function setDestinatario (address to) public soloAdmin{
+      // Imposta il destinatario
       destinatario=to;
     }
 
-    function payItem(uint256 price, uint itemId) public returns (uint){
+   
+    // Funzione per pagare un elemento
+function payItem(uint256 price, uint itemId) public returns (uint){
+      // Controlla che il prezzo memorizzato per l'elemento corrisponda a quello specificato
       require(pricesMapping[itemId]==price);
-      //Qua va messo l'indirizzo del gestore della CryptoGym
-      require(transfer(0xc9a68AA2B9dF277C8BEA55187a1FC4B89269c2e4,price));
+      // Controlla che il pagamento sia stato eseguito con successo all'indirizzo del gestore della CryptoGym
+      require(transfer(0x78f247fe3ccee2AbB9eA8a354f186cE5EB1dF538,price));
+      // Restituisce l'ID dell'elemento
       return itemId;
     }
 
